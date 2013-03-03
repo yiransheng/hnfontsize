9e95c0ac296f23bb05c5f3dca1233b1cc4beadfb
hyenagrins <me@yiransheng.com>
2013-03-03 06:42:01 -0500

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+Hacker News Font Size Controller
+-------------------------------
+
+I have used a couple of custom style sheets for [http://news.ycombinator.com](http://news.ycombinator.com) in the past, and eventually found
+that the default look to be the best: minimal and good readbility. However, the font size on hacker news appears too small on my most recent computer. 
+Instead of getting a new "theme", I though it'd be nice to only change this one thing, and ideally I should be able to adjust it on the go. So I hacked together this Chrom Extension. 
+
+It allows the user to drag a controller and resize all fonts proportionally to their original size, and remembers the setting to localStorage. Hopefully this can help some hn reader with the same problem as well.  
+
+Screen Shot:
+============
+![screenshot](http://i.imgur.com/XZdelvX.jpg)
---
+++ src/hnfontsize.js
@@ -0,0 +1,138 @@
+(function () {
+
+    // ignore certain pages
+    var page = document.URL.substr(document.URL.lastIndexOf('/') + 1);
+    page = page.replace(/\?\S*$/, "");
+    if (/^(submit|x|login)$/i.test(page)) return;
+
+
+    // convenience functions
+    var _each = Array.prototype.forEach ? function(arraylike, iterator) {
+            Array.prototype.forEach.call(arraylike, iterator);    
+            return arraylike
+        } : function(arraylike, iterator) {
+            for (var i=0, ilen=arraylike.length; i<ilen; i++) {
+                iterator(arraylike[i], i);
+            }
+            return arraylike
+        };
+
+    var _map = function (arr, iterator) {
+        if (arr.map) {
+            return arr.map(iterator);
+        }
+        var results = [];
+        _each(arr,function(x,i,a){
+            results.push(iterator(x,i,a));
+        });
+        return results; 
+    };
+
+    var _reduce = function (arr, iterator, memo) {
+        if (arr.reduce) {
+            return arr.reduce(iterator, memo);
+        }
+        _each(arr, function (x, i, a) {
+            memo = iterator(memo, x, i, a);
+        });
+        return memo;
+    };
+
+    // where to put the it, hn header is a table, put the thingi
+    // inside first <tr>
+    var getContainer = function() {
+        return document.getElementsByTagName("tbody")[1].firstChild;
+    };
+
+    // finding current style property of a node
+    var getStyle = function (el,styleProp){
+        var y;
+        if (window.getComputedStyle)
+            y = window.getComputedStyle(el,null)[styleProp];
+        else if (el.currentStyle) y = el.currentStyle[styleProp];
+        return y;
+    }
+
+
+    var hn_classes = ["title", "subtext", "comment", "comhead"];
+
+    var fontsetting = {factor : localStorage.getItem("font_adjustment") || 100};
+
+    _each(hn_classes, function(_class) {
+        var child,nodelist = document.getElementsByClassName(_class),fs;
+        if (nodelist.length) {
+            child = nodelist[0];
+            while (!(fs=getStyle(child, "fontSize"))) {
+                child = child.firstChild;
+            }
+            fontsetting[_class] = parseInt(fs);
+        }
+    });
+
+    // rewrite a style element for updating font size changes
+    var overrideStyle = function() {
+        var y = "";
+        _each(hn_classes, function(_class){
+            y += (
+                "." + _class
+                + '{font-size:'
+                + fontsetting[_class]*fontsetting.factor/100
+                + "px}"
+                );
+        });             
+        return y
+    };
+
+    var controller = document.createElement("td");
+    controller.setAttribute("style", "position: relative; top: -5px; font-weight: bold; font-size: 13px; color: #fff; display: inline; text-align:right;");
+    controller.innerHTML =
+        '<input type="range" min="80" max="250" style = "position:relative;top: 2px; width: 50%; max-width: 250px; height: 10px;" value="'
+        + fontsetting.factor 
+        + '"><span>'
+        + fontsetting.factor
+        + '% </span><img src="'
+        + chrome.extension.getURL("font_size.gif")
+        + '" style="position:relative; top: 6px;"/>'
+
+    var container = getContainer();
+    container.insertBefore(controller, container.lastChild);
+
+    var new_style = document.createElement("style");
+    document.body.appendChild(new_style);
+
+    // wrapper for mouse events callback, so than mousemove dont 
+    // trigger dom updates too often
+    var adjustFont = function(limit) {
+        var now, 
+            lastTimeStamp,
+            operating;
+
+        return function(e) {
+            e.stopPropagation();
+            now = Date.now();
+            if (e.type == "mousedown") {
+                operating = true;
+            } 
+            if (operating && (!lastTimeStamp || now - lastTimeStamp > limit)) {
+                lastTimeStamp = now;
+                fontsetting.factor = +e.target.value;
+                new_style.innerText = overrideStyle();
+                e.target.nextSibling.innerText = fontsetting.factor + "% ";
+            }
+            if (e.type == "mouseup") {
+                operating = false;
+                lastTimeStamp = null;
+            }
+        }
+    };
+
+    var meter = controller.firstChild;
+    meter.onmousedown = meter.onmouseup = meter.onmousemove = adjustFont(100);
+
+    if (fontsetting.factor !== 100) {
+        new_style.innerText = overrideStyle();
+        meter.nextSibling.innerText = fontsetting.factor + "% ";
+    }
+
+
+})();
